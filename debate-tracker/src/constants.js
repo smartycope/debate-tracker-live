0e940e179a06ae9ce17109d48977af350aed47a5
Copeland Carter <smartycope@gmail.com>
2024-03-08 20:04:33 -0700

    Moved from localhost to smartycope.pythonanywhere.com

--- debate-tracker/src/constants.js
+++ debate-tracker/src/constants.js
@@ -1,2 +1,3 @@
 // const API_URL = "http://localhost:8000/api/";
-export const API_URL = "http://127.0.0.1:8000/api/";
+// export const API_URL = "http://127.0.0.1:8000/api/";
+export const API_URL = "smartycope.pythonanywhere.com/api/";
